27639074544e6b7c61f0fca56b212fe9b87a9607
Yash <163098540+yashportfolio@users.noreply.github.com>
2025-03-25 12:20:56 +0530

    Delete mdiskplay.com directory

--- mdiskplay.com/_next/static/d_HGZgYekiPo_ptfC0n91/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Foffer\u002F[name]","\u002Freader\u002F[id]","\u002Fshorts\u002F[id]","\u002Fterabox\u002F[id]","\u002Fvstore\u002F[id]","\u002Fy\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
